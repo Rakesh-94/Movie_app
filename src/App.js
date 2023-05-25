24c9b33328a7b375957d09ef6ce9d54146387b5f
Rakesh-94 <rakeshkrpal861@gmail.com>
2023-05-25 17:50:38 +0530

    Add some in homePage functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home';
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import logo from "../../logo.png"
 import {Link} from "react-router-dom"
+import { ImSearch } from "react-icons/im"
+
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className='header'>
+
         <img src={logo} alt="logo" />  
 
         <div>
             <Link to= "/tvshows">TV Shows</Link>
+            <Link to= "/movies">Movies</Link>
+            <Link to= "/recently added">Recently Added</Link>
+            <Link to= "/my list">My List</Link>
         </div>
+        <ImSearch />
     </nav>
   )
 }
